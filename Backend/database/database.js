e4ee36a2895c9e10ec21fc8194bf005abfb9de99
Paolo Tomatis <tomatis.pol@gmail.com>
2025-08-22 14:25:47 +0200

    Test per Risoluzione Bug per Deploy 3

--- Backend/database/database.js
+++ Backend/database/database.js
@@ -1,6 +1,9 @@
 // Importazione moduli
 import { createPool } from 'mysql2';
 import { configDotenv } from 'dotenv';
+import fs from 'fs';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 // Configurazione .env
 configDotenv();
@@ -8,9 +11,11 @@ configDotenv();
 // Creazione connessione al database
 const pool = createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
-    password: process.env.DB_PSW,
-    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: isProduction ? { ca: fs.readFileSync('./ca.pem') } : undefined,
 }).promise();
 
 // Esportazione della connessione
